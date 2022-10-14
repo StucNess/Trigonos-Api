a7d1afd1a8ba6201830514c970eff8ba51f50559
Elvis <ejoocontactos@gmail.com>
2022-10-14 15:20:14 -0300

    14-10 Api Banks

---
+++ TrigonosEnergyWebAPI/Controllers/BanksController.cs
@@ -0,0 +1,32 @@
+﻿using AutoMapper;
+using Core.Entities;
+using Core.Interface;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using TrigonosEnergy.Controllers;
+using TrigonosEnergyWebAPI.DTO;
+
+namespace TrigonosEnergyWebAPI.Controllers
+{
+    [ApiExplorerSettings(GroupName = "APIBanks")]
+    public class BanksController : BaseApiController
+    {
+        private readonly IGenericRepository<CEN_banks> _banksRepository;
+        private readonly IMapper _mapper;
+        public BanksController(IGenericRepository<CEN_banks> banksRepository, IMapper mapper)
+        {
+            _banksRepository = banksRepository;
+            _mapper = mapper;
+        }
+
+        [HttpGet]
+        [ProducesResponseType(400)]
+        [ProducesDefaultResponseType]
+        public async Task<ActionResult<IReadOnlyList<BanksDto>>> GetBanks()
+        {
+            var datos = await _banksRepository.GetAllAsync();
+            //var maping = _mapper.Map<IReadOnlyList<CEN_banks>, IReadOnlyList<BanksDto>>(datos);
+            return Ok(datos);
+        }
+    }
+}
---
+++ TrigonosEnergyWebAPI/DTO/BanksDto.cs
@@ -0,0 +1,11 @@
+﻿namespace TrigonosEnergyWebAPI.DTO
+{
+    public class BanksDto
+    {
+        public int? Id { get; set; }
+        public string? Code { get; set; }
+        public string? Name { get; set; }
+        public int? Sbif { get; set; }
+        public int? Type { get; set; }
+    }
+}
