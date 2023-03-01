495361da0f04a9b23ba9ff24e6ff9a9e21e72cc0
Elvis <ejoocontactos@gmail.com>
2023-03-01 17:26:45 -0300

    SAAD

---
+++ LogicaTrigonos/Data/Configuration/TrgnsUserProyectsConfiguration.cs
@@ -0,0 +1,23 @@
+﻿using Core.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LogicaTrigonos.Data.Configuration
+{
+    internal class TrgnsUserProyectsConfiguration: IEntityTypeConfiguration<TRGNS_UserProyects>
+    {
+
+        public void Configure(EntityTypeBuilder<TRGNS_UserProyects> builder)
+        {
+            builder.HasOne(p => p.cEN_Participants).WithMany().HasForeignKey(i => i.idProyect);
+
+
+
+        }
+    }
+}
