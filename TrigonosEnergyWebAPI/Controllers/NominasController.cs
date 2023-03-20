2799b1f122919e24494141802e586de6b2034809
Elvis <ejoocontactos@gmail.com>
2023-03-20 16:44:40 -0300

    Empezando controller nominas

---
+++ TrigonosEnergyWebAPI/Controllers/NominasController.cs
@@ -0,0 +1,27 @@
+﻿using AutoMapper;
+using Core.Entities;
+using Core.Interface;
+using Microsoft.AspNetCore.Mvc;
+using TrigonosEnergy.Controllers;
+using TrigonosEnergyWebAPI.DTO;
+
+namespace TrigonosEnergyWebAPI.Controllers
+{
+    public class NominasController:BaseApiController
+    {
+        private readonly IMapper _mapper;
+        private readonly IGenericRepository<TRGNS_Datos_Facturacion> _instruccionesRepository;
+        //private readonly IGenericRepository<>
+        public NominasController(IMapper mapper, IGenericRepository<TRGNS_Datos_Facturacion> instruccionesRepository)
+        {
+            _mapper= mapper;
+            _instruccionesRepository= instruccionesRepository;
+        }
+        [HttpGet]
+        
+        public async Task<ActionResult<Pagination<InstruccionesDTO>>> GetInstructionsOpen(int id, [FromQuery] Instru)
+        {
+
+        }
+    }
+}
