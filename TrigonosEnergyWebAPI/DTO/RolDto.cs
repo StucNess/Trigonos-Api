6c22d6bc1a4abd6bc7e25274bbc2faa2108bafe8
Andres <edgar.proboste@gmail.com>
2023-04-25 18:33:36 -0400

    cambio en strng

--- TrigonosEnergyWebAPI/DTO/RolDto.cs
+++ TrigonosEnergyWebAPI/DTO/RolDto.cs
@@ -2,7 +2,7 @@
 {
     public class RolDto
     {
-        public int Id { get; set; }
+        public string Id { get; set; }
         public string Name { get; set; }
         public string Descripcion { get; set; }
         public int Bhabilitado { get; set; }
